3d9739560f64cdc0945da282a1bc81c780f3836c
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-08-03 09:56:06 +0530

    Styles Button TouchableOpacity Text

--- App.tsx
+++ App.tsx
@@ -1,118 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  Button,
+  ToastAndroid,
+  Alert,
   View,
+  TouchableOpacity
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="Sachira Madhushan">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <Text style={style.text}>Sacheera</Text>
+      <Button title='click me'onPress={showToast}></Button>
+      <View style={style.view}>
+        <Button title='click me'onPress={showAlert}></Button>
+      </View>
+      <TouchableOpacity style={style.touchable}>
+        <Text style={style.text}>Click Here</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const showToast=()=>{
+  ToastAndroid.showWithGravity(
+    "Button Click",
+    ToastAndroid.LONG,
+    ToastAndroid.BOTTOM
+  );
+}
+const showAlert=()=>{
+  Alert.alert("Success","Do you agree?",[
+    {
+      text:"Ok",
+      onPress:()=>{},
+    },
+    {
+      text:"Cansel",
+      onPress:()=>{}
+    }
+  ]
+  )
+}
+const style=StyleSheet.create({
+  text:{
+    color:'white',
+    fontSize:20,
+    textAlign:'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  view:{
+    margin:50
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  touchable:{
+    backgroundColor:'blue',
+    padding:10,
+    margin:50,
+    borderRadius:15,
+  }
+})
 
 export default App;
